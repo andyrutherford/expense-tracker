587e720165ea8c8bc24189f4b70318b97ca01250
Andrew <andrew.rutherford.6@gmail.com>
2020-05-01 17:54:31 +0700

    show current balance and balance history items

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const Balance = () => {
+  const context = useContext(GlobalContext);
+  const { transactions } = context;
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
   return (
     <>
       <h4>Your Balance</h4>
-      <h1>$0.00</h1>
+      <h1>${total}</h1>
     </>
   );
 };
