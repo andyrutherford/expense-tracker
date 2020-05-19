77de3587cda2b64462e9707a747da8f967c56e70
Andy <andrew.rutherford.6@gmail.com>
2020-05-19 12:22:31 +0700

    transaction amounts no longer include cents

--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -1,3 +1,7 @@
 export function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return x
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+    .split('.')
+    .slice(0, 1);
 }
